c75d309b5c9d3aac0c64e19639026474d52122e6
jhasandeep <skjsan11@gmail.com>
2024-12-08 13:10:31 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
